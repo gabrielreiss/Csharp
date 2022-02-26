58bfb66d139d7f1cf650a8705643980da33c42b1
gabrielreiss <gabrielreissdecastro@gmail.com>
2022-02-26 17:34:56 -0300

    exercicios beecrowd

---
+++ curso C#/06 - funcoes.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ExibirMsg();
+            ExibirMsg();
+            ExibirMsg();
+            Console.ReadLine();
+        }
+
+        static void ExibirMsg()
+        {
+            Console.WriteLine("Bem vindo!");
+            Console.WriteLine("Utilizando funções");
+
+        }
+    }
+}
