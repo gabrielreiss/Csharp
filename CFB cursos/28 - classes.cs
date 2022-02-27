456bdec5ea1810617c15273fa81261f5cd48a8d5
gabrielreiss <gabrielreissdecastro@gmail.com>
2022-02-27 13:54:07 -0300

    classes

--- CFB cursos/28 - classes.cs
+++ CFB cursos/28 - classes.cs
@@ -1,3 +1,5 @@
+//aulas 28 a 30
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,52 @@ namespace CFBcursos
 {
     class Program
     {
+        public class Jogador{
+            public int energia;
+            public bool vivo = true;
+            public string nome;
+            //metodo construtor mesmo nome da classe
+            public Jogador(){
+                nome = "Jogador";
+                energia = 100;
+                vivo = true;
+            }
+            public Jogador(string n){
+                nome = n;
+                energia = 100;
+                vivo = true;
+            }
+            public Jogador(string n, int e){
+                nome = n;
+                energia = e;
+                vivo = true;
+            }
+            public Jogador(string n, int e, bool v){
+                nome = n;
+                energia = e;
+                vivo = v;
+            }        
+            //metodo destrutor
+            ~Jogador(){
+                Console.WriteLine("Jogador {0} foi destruido", nome);
+            }
+            public void info(){
+                Console.WriteLine("Nome do jogador {0}", nome);
+                Console.WriteLine("Energia do jogador {0}", energia);
+                Console.WriteLine("Status do jogador {0}\n", vivo);
+            }
+        }
         static void Main(string[] args)
         {
-            
+            Jogador j1 = new Jogador();
+            Jogador j2 = new Jogador("Joao");
+            Jogador j3 = new Jogador("Carlos", 70);
+            Jogador j4 = new Jogador("Demetrio", 30, true);
+
+            j1.info();
+            j2.info();
+            j3.info();
+            j4.info();
 
             Console.ReadLine();
         }
---
+++ CFB cursos/Referências.md
@@ -0,0 +1,3 @@
+# Referência
+
+[Link](https://www.youtube.com/watch?v=dVzJ3bx68FA&list=PLx4x_zx8csUglgKTmgfVFEhWWBQCasNGi)
