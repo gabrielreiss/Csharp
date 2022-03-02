7dee3b3c531962b001f377b954ae1ab5ba13ab60
gabrielreiss <gabrielreissdecastro@gmail.com>
2022-03-01 21:30:47 -0300

    exercicios

--- exercism/csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ exercism/csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,20 @@
 class Lasagna
 {
     // TODO: define the 'ExpectedMinutesInOven()' method
-
+    ExpectedMinutesInOven(){
+        return 40;
+    }
     // TODO: define the 'RemainingMinutesInOven()' method
-
+    RemainingMinutesInOven(int valor){
+        return 40 - valor;
+    }
     // TODO: define the 'PreparationTimeInMinutes()' method
-
+    PreparationTimeInMinutes(int valor){
+        return valor * 2;
+    }
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    ElapsedTimeInMinutes(int layers, int oven){
+        return layers * 2 + oven;
+    }
+
 }
