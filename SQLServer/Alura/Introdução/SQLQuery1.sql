d3cdad713ed4009da4504425f06d4c338222005d
gabrielreiss <gabrielreissdecastro@gmail.com>
2022-03-06 08:19:19 -0300

    Sql server praticando querys

---
+++ SQLServer/Alura/Introdução/SQLQuery1.sql
@@ -0,0 +1,11 @@
+select *
+from [dbo].[MSreplication_options]
+;
+
+select *
+from dbo.spt_fallback_db
+;
+
+/* forçando erro
+SELECT * FROM [dbo].[MSreplication_optionsXXXX];
+*/
---
+++ SQLServer/Alura/Introdução/SQLQuery2.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE sucos_vendas_01;
